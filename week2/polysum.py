41e0e3d5540dfc45c84dbd4ad3e3540b37375ca8
dmp <dmp@dmp.dmp>
2016-09-30 22:50:28 +0200

    final version

--- week2/polysum.py
+++ week2/polysum.py
@@ -4,6 +4,10 @@ This function sums the area and square of the perimeter of the regular polygon.
 The function returns the sum, rounded to 4 decimal places.
 Input parameters: n - number of polygon sides
                   s - length of polygon side
+Functions: area - computes area of the polygon
+           perimeter - computes perimeter of the polygon
+
+
 """
 def area(n,s):
     return (0.25 * n * s * s)/(math.tan(math.pi/n))
